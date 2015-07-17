bbc86cc03076f6b784effb9a9bfb4719d2444d07
nugycal <zavery011@ZAVERY011_2017.hillsgrammar.nsw.edu.au>
2015-07-17 13:00:23 +1000

    Revert "Revert "collision detection""
    This reverts commit 1c2cb2dcbc277b4e55b6e314a2a4dc8e0334da20.

--- SifiGame/SifiGame/js/Platform.js
+++ SifiGame/SifiGame/js/Platform.js
@@ -1,4 +1,4 @@
-﻿function platform(index, x, y, width, height, type, ax, ay, bx, by) {
+function platform(index, x, y, width, height, type, ax, ay, bx, by) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -36,8 +36,8 @@
 
         case PLATFORMTYPE_PILLAR1:
             this.width = 40;
-            this.height = 20;
-            ctx.drawImage(wall1, this.x, this.y - (100 - (this.height)) + 20);
+            this.height = 26;
+            ctx.drawImage(wall1, this.x, this.y - (100 - this.height));
             break;
 
         case PLATFORMTYPE_SMALL_BOX:
--- SifiGame/SifiGame/js/collision_detection.js
+++ SifiGame/SifiGame/js/collision_detection.js
@@ -1,4 +1,5 @@
-﻿function collisiondetection() {
+function collisiondetection() {
+    
     this.switch1 = 0;
 
 
@@ -38,7 +39,7 @@
 
     this.test5 = function (player, platform) {
         var result = false;
-        if (player.y > platform.y) {
+        if (player.y + (player.height-10)> platform.y) {
             result = true;
         }
         return result;
@@ -46,7 +47,7 @@
 
     this.test6 = function (player, platform) {
         var result = false;
-        if (player.y + player.height > platform.y) {
+        if (player.y + (player.height-10)+ 10 > platform.y) {
             result = true;
         }
         return result;
@@ -61,7 +62,7 @@
     }
     this.test8 = function (player, platform) {
         var result = false;
-        if (player.y > platform.y + platform.height) {
+        if (player.y + (player.height-10) > platform.y + platform.height) {
             result = true;
         }
         return result;
@@ -343,7 +344,7 @@
             if (player.yvel < 0) {
                 player.yvel = 0;
             }
-            player.y = platform.y + platform.height - 1;
+            player.y = platform.y + platform.height - 1-(player.height-10);
         }
         if (collisiondetection1.collision(player, platform) == platformside.top) {
             //console.log("Collision")
