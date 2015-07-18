f0931b0d62d13bf59ac3b8bbba1d12bf5f4e9fd7
nugycal <nugycal@gmail.com>
2015-07-18 20:33:29 +1000

    Fixed Bug

--- SifiGame/SifiGame/js/game.js
+++ SifiGame/SifiGame/js/game.js
@@ -288,7 +288,6 @@ function game() {
             if (collisiondetection1.testcollision(playercollection.array[0], platformcollection.array[platformcounter]) && platformcollection.array[platformcounter].removable && playercollection.array[0].money >= platformcollection.array[platformcounter].price) {
                 ctx.fillText("'E' to purchase this " + platformcollection.array[platformcounter].type, this.canvastranslatex + cwidth / 2 - ctx.measureText("'E' to purchase this door").width/2, this.canvastranslatey + 50);
                 ctx.fillText("$" + platformcollection.array[platformcounter].price, this.canvastranslatex + cwidth / 2 - ctx.measureText("$" + platformcollection.array[platformcounter].price).width / 2, this.canvastranslatey + 70);
-                ctx.fillText(platformcollection.array[platformcounter].message, this.canvastranslatex + cwidth / 2 - ctx.measureText(platformcollection.array[platformcounter].message).width / 2, this.canvastranslatey + 90);
             }
             if (collisiondetection1.testcollision(playercollection.array[0], platformcollection.array[platformcounter]) && platformcollection.array[platformcounter].removable && playercollection.array[0].money < platformcollection.array[platformcounter].price) {
                 ctx.fillText("Insufficient Funds", this.canvastranslatex + cwidth / 2 - ctx.measureText("Insufficient Funds").width/2, this.canvastranslatey + 50);
